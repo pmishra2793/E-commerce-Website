2e95552f2ae518788796ae3071a175e167058d14
pappu mishra <pappu020793@gmail.com>
2019-07-12 18:39:50 +0530

    first commit

---
+++ shop/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def tracker(request):
+    return render(request,'tracker.html')
+
+def search(request):
+    return render(request,'search.html')
+
+def productview(request):
+    return render(request,'productview.html')
+
+def checkout(request):
+    return render(request,'checkout.html')
+
+
