530c7d12464796780ffe1062908693658967f185
pappu mishra <pappu020793@gmail.com>
2019-07-16 18:28:35 +0530

    template inheritance

--- shop/views.py
+++ shop/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
+from math import ceil
 
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    products = Product.objects.all()
+    print (products)
+    n = len(products)
+    nSlides = n//4 + ceil((n/4)-(n//4))
+    params = {'no_of_slide':nSlides,'range':range(1,nSlides),'product':products}
+    return render(request,'index.html',params)
 
 def about(request):
     return render(request,'about.html')
